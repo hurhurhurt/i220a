a396cd6d086d8fdcb7828f66f71619c0067934c7
Thomas <thur1@binghamton.edu>
2019-09-10 14:14:54 -0400

    i wanna die

--- work/prj1-sol/bits-to-ints.c
+++ work/prj1-sol/bits-to-ints.c
@@ -1,4 +1,5 @@
 
+
 #include "bits-to-ints.h"
 #include "errors.h"
 #include <string.h>
@@ -45,12 +46,6 @@
  *  function should return with *isEof set to true.
  */
 
-void append(char* s, char c){
-  int len = strlen(s);
-  s[len] = c;
-  s[len+1] = '\0';
-}
-
 int allocate_space(FILE *inFile){
   int counter = 0;
   while (!feof(inFile)){
@@ -69,8 +64,7 @@ int allocate_space(FILE *inFile){
       }
     }  
   }
-  fclose(inFile);
-  return counter -  1; // accounts for the extra EOF character
+  return counter; // accounts for the extra EOF character
 }
 
 BitsValue
@@ -79,58 +73,29 @@ bits_to_ints(FILE *inFile, const char *inName, int nBits, bool *isEof)
   //nBits value should make sense
   assert(0 < nBits && nBits <= CHAR_BIT*sizeof(BitsValue));
   BitsValue value = 0;
-  printf("%d", allocate_space(inFile));
-  *isEof = true;
-  return value;
-  /*
-  // allocate enough space for array
   
-  while (!feof(inFile)){
-    if (inFile == NULL){
-      printf("Invalid File");
-      return 0;
+  long int space = allocate_space(inFile);
+  printf("%ld\n", space);
+  
+  char returnString[space];
+  rewind(inFile);
+  
+  int c = fgetc(inFile);
+  int i = 0;
+  while (c != EOF){
+    if (c == 10 || c == 32){
+      c = fgetc(inFile);
     }
-    int c = fgetc(inFile);
-    
-    if (c == EOF){
-      printf("End of file.");
-      break;
-      }
-    if (c != 10){
-      if (c == 32){
-	c = fgetc(inFile);
-      }
-      else{
-	append(str, c);
-	c = fgetc(inFile);
-      }   
+    //sprintf(returnString, "%c", c);
+    else{
+      returnString[i] = c;
+      i++;
+      c = fgetc(inFile);
     }
   }
+  printf("%s\n", returnString);
+  
   fclose(inFile);
   *isEof = true;
-
-  printf("\n Contents of file: %s", str);
-  printf("\n Length of string: %ld\n", sizeof(str));
-  
-  char newArray[8][8];
-  int count = 0;
-  for (int z = 0;
-    for (int i = 0; i < 8; i++){
-      for (int j = 0; j < 8; j++){
-        if (count < 8){
-	newArray[j][i] = 
-      
-  
-  int counter = 0;
-  for (int i = 0; i < strlen(str); i++){
-    if (counter < 8){
-      printf("%c", str[i]);
-      counter++;
-    }
-    else{
-      printf("%c\n", str[i]);
-      counter = 0;
-	}
-  }
-  */
+  return value;
 }
