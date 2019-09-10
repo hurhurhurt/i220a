4c6339ac09dfe0f9533326911cead42ab73feb28
Thomas <thur1@binghamton.edu>
2019-09-10 10:45:04 -0400

    temp commit

--- work/prj1-sol/bits-to-ints.c
+++ work/prj1-sol/bits-to-ints.c
@@ -1,3 +1,4 @@
+
 #include "bits-to-ints.h"
 #include "errors.h"
 #include <string.h>
@@ -44,44 +45,81 @@
  *  function should return with *isEof set to true.
  */
 
-unsigned long long int toByte(char *bits){
-  unsigned int sum = 0;
-  for (int i = 0; i < 8; i++){
-    sum <<=1;
-    sum += bits[i] - '0';
-  }
-  return sum;
-  
+void append(char* s, char c){
+  int len = strlen(s);
+  s[len] = c;
+  s[len+1] = '\0';
 }
+
+
 BitsValue
 bits_to_ints(FILE *inFile, const char *inName, int nBits, bool *isEof)
-{
+{ 
   //nBits value should make sense
   assert(0 < nBits && nBits <= CHAR_BIT*sizeof(BitsValue));
   BitsValue value = 0;
+
+  // allocate enough space for array
+  long temp = 0;
+  if (inFile){
+    fseek(inFile, 0, SEEK_END);
+    long fileSize = ftell(inFile);
+    fseek(inFile, 0, SEEK_SET);
+    temp = fileSize;
+  }
+  //printf("%ld", temp /2 + 1);
+  char str[(temp / 2) + 1];
+  str[(temp / 2) + 1] = '\0';
+  //convert to byte array
   while (!feof(inFile)){
     if (inFile == NULL){
       printf("Invalid File");
       return 0;
     }
-    char str[8];
     int c = fgetc(inFile);
-    if  (c != 10){
+    /*
+    if (c == EOF){
+      printf("End of file.");
+      break;
+      }*/
+    if (c != 10){
       if (c == 32){
 	c = fgetc(inFile);
       }
       else{
-	sprintf(str," %c", c);
+	append(str, c);
 	c = fgetc(inFile);
       }   
     }
-    printf("%s", str);
-    /*
-    int total = toByte(str);
-    printf("%d", total);*/
   }
   fclose(inFile);
   *isEof = true;
+  //printf("\n Contents of file: %s", str);
+  //printf("\n Length of string: %ld\n", sizeof(str));
+
+  char newArray[8][8];
+  int count = 0;
+  for (int z = 0;
+    for (int i = 0; i < 8; i++){
+      for (int j = 0; j < 8; j++){
+        if (count < 8){
+	newArray[j][i] = 
+      
+      
+
+
+  
+  int counter = 0;
+  for (int i = 0; i < strlen(str); i++){
+    if (counter < 8){
+      printf("%c", str[i]);
+      counter++;
+    }
+    else{
+      printf("%c\n", str[i]);
+      counter = 0;
+	}
+  }
+  
   return value;
 }
-
