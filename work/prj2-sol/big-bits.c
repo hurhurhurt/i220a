df686a45f83c00b3979c4881ff2b2a6cb53b75cc
Thomas <thur1@binghamton.edu>
2019-10-11 00:58:51 -0400

    prj2 partial commit

--- work/prj2-sol/big-bits.c
+++ work/prj2-sol/big-bits.c
@@ -26,13 +26,13 @@ struct BigBits {
  *
  *  Returns NULL on error with errno set "appropriately".
  */
-const BigBits *
-newBigBits(const char *hex)
+
+const BigBits * newBigBits(const char *hex)
 {
   assert(CHAR_BIT == 8);
-  int sizeHex = sizeof(hex);
+  int sizeHex = strlen(hex);
   struct BigBits * convertBits = malloc(sizeHex*sizeof(struct BigBits));
-  convertBits->arr = malloc(sizeHex);
+  convertBits->arr = malloc(sizeHex * (sizeof(int)));
   convertBits->size = sizeHex;
   for (int i = 0; i < sizeHex; i++){
     convertBits->arr[i] = charToHexet(hex[i]);
@@ -46,8 +46,8 @@ newBigBits(const char *hex)
 void
 freeBigBits(BigBits *bigBits)
 {
-  free(bigBits->arr);
-  free(bigBits);
+  //free(bigBits->arr);
+  //free(bigBits);
 }
 
 
@@ -56,13 +56,17 @@ freeBigBits(BigBits *bigBits)
  *  Returns NULL on error with errno set "appropriately".  (Note that
  *  there is no call to free the corresponding string).
  */
-const char *
-stringBigBits(const BigBits *bigBits)
+const char * stringBigBits(const BigBits *bigBits)
 {
-  //@TODO
-  return NULL;
+  int size = bigBits->size;
+  char * temp = malloc(size*sizeof(char));
+  
+  for (int i = 0; i < size; i++){
+    if(hexetToChar(bigBits->arr[i]) < 0) return NULL;
+    temp[i] = hexetToChar(bigBits->arr[i]);
+  }
+  return temp;
 }
-
 
 /** Return a new BigBits which is the bitwise-& of bigBits1 and bigBits2.
  *  Returns NULL on error with errno set "appropriately".
@@ -70,8 +74,20 @@ stringBigBits(const BigBits *bigBits)
 const BigBits *
 andBigBits(const BigBits *bigBits1, const BigBits *bigBits2)
 {
-  //@TODO
-  return NULL;
+  int smaller, diff; //smaller tracks size of small BigBits, diff tracks difference so i can shift over when bitwise anding
+  const BigBits * bigBit = bigBits1;
+  const BigBits * smallBit = bigBits2;
+  if (bigBit->size >= smallBit->size) smaller = smallBit->size;
+  else {
+    smaller = bigBit->size;
+    bigBit = bigBits2;
+    smallBit = bigBits1;
+  }
+  diff = bigBit->size - smallBit->size;
+  for (int i = 0; i < smaller; i++){
+    smallBit->arr[i] &= bigBit->arr[i+diff];
+  }
+  return smallBit;
 }
 
 /** Return a new BigBits which is the bitwise-| of bigBits1 and bigBits2.
@@ -80,8 +96,21 @@ andBigBits(const BigBits *bigBits1, const BigBits *bigBits2)
 const BigBits *
 orBigBits(const BigBits *bigBits1, const BigBits *bigBits2)
 {
-  return bigBits1->arr | bigBits2->arr;
-  return NULL;
+  int bigger, diff;
+  const BigBits * bigBit = bigBits1;
+  const BigBits * smallBit = bigBits2;
+  if (bigBit->size >= smallBit->size) bigger = bigBit->size;
+  else{
+    bigger = smallBit->size;
+    bigBit = bigBits2;
+    smallBit = bigBits1;
+  }
+  diff = bigBit->size - smallBit->size;
+  for (int i = 0; i < bigger; i++){
+    bigBit->arr[i+diff] |= smallBit->arr[i];
+  }
+  return bigBit;
+    
 }
 
 /** Return a new BigBits which is the bitwise-^ of bigBits1 and bigBits2.
@@ -90,6 +119,18 @@ orBigBits(const BigBits *bigBits1, const BigBits *bigBits2)
 const BigBits *
 xorBigBits(const BigBits *bigBits1, const BigBits *bigBits2)
 {
-  //@TODO
-  return NULL;
+  int bigger, diff;
+  const BigBits * bigBit = bigBits1;
+  const BigBits * smallBit = bigBits2;
+  if (bigBit->size >= smallBit->size) bigger = bigBit->size;
+  else{
+    bigger = smallBit->size;
+    bigBit = bigBits2;
+    smallBit = bigBits1;
+  }
+  diff = bigBit->size - smallBit->size;
+  for (int i = 0; i < bigger; i++){
+    bigBit->arr[i+diff] ^= smallBit->arr[i];
+  }
+  return bigBit;
 }
